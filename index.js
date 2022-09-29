cf650d8aaf20bba526d0d93f9a4ac60773fc3f0b
mithrayls <mithray@protonmail.com>
2022-09-30 01:43:45 +1300

    after eslint

--- index.js
+++ index.js
@@ -1,70 +1,65 @@
-const parsePath           = require("parse-path")
-const path                = require("path")
-const memoizeWith         = require("ramda/src/memoizeWith.js")
-const jsYaml              = require("js-yaml")
-const dagCbor             = require("@ipld/dag-cbor")
-const undici              = require("undici")
-const fs                  = require("fs")
-const shelljs             = require("shelljs")
+const parsePath = require('parse-path')
+const path = require('path')
+const memoizeWith = require('ramda/src/memoizeWith.js')
+const jsYaml = require('js-yaml')
+const dagCbor = require('@ipld/dag-cbor')
+const undici = require('undici')
+const fs = require('fs')
+const shelljs = require('shelljs')
 
-const R =
-  (f) => memoizeWith(String,require("ramda/src/" + f + ".js"))
+const R = (f) => memoizeWith(String, require(`ramda/src/${f}.js`))
 
-const readLocalFile =
-  (filePath) => fs.readFileSync(filePath)
+const readLocalFile = (filePath) => fs.readFileSync(filePath)
 
-const readRemoteFile = 
-  R("pipe")
-  ( undici.request
-  , R("andThen")( (x) => x.body.text() )
-  )
+const readRemoteFile = R('pipe')(
+  undici.request,
+  R('andThen')((x) => x.body.text())
+)
 
-const getDocument =
-  R("ifElse")
-    ( x => R("equals")(parsePath(x)["protocol"], "file")
-    , async x => (
-      { content: await readLocalFile(x)
-      , extname: path.extname(x)
-      })
-    , async x => (
-      { content: await readRemoteFile(x)
-      , extname: path.extname(x)
-      })
-    )
+const getDocument = R('ifElse')(
+  (x) => R('equals')(parsePath(x).protocol, 'file'),
+  async (x) => (
+    {
+      content: await readLocalFile(x),
+      extname: path.extname(x)
+    }),
+  async (x) => (
+    {
+      content: await readRemoteFile(x),
+      extname: path.extname(x)
+    })
+)
 
-const parseDocument =
-  R("andThen")
-    ( R("cond")(
-      [ [ x => [".yaml",".yml"].includes(x.extname)
-        , x => jsYaml.load(x.content)
-        ]
-      , [ x => [".json"].includes(x.extname)
-        , x => JSON.parse(x.content)
-        ]
-      , [ x => [".cbor"].includes(x.extname)
-        , x => dagCbor.decode(x.content)
-        ]
-      , [ x => [".dhall"].includes(x.extname)
-        , x => { 
-            const access = fs.createWriteStream('/dev/null')
-            const stdwriteOriginal = process.stdout.write
-            process.stdout.write = process.stderr.write = access.write.bind(access)
-            const { err, stdout, stderr } = shelljs.echo(x.content).exec("dhall-to-json",{silent: true})
-            process.stdout.write = process.stderr.write = stdwriteOriginal
-            return (JSON.parse(stdout))
-          }
-        ]
-      ])
-    )
+const parseDocument = R('andThen')(R('cond')(
+  [[(x) => ['.yaml', '.yml'].includes(x.extname),
+    (x) => jsYaml.load(x.content)
+  ],
+  [(x) => ['.json'].includes(x.extname),
+    (x) => JSON.parse(x.content)
+  ],
+  [(x) => ['.cbor'].includes(x.extname),
+    (x) => dagCbor.decode(x.content)
+  ],
+  [(x) => ['.dhall'].includes(x.extname),
+    (x) => {
+      const access = fs.createWriteStream('/dev/null')
+      const stdwriteOriginal = process.stdout.write
+      process.stdout.write = process.stderr.write = access.write.bind(access)
+      const { stdout } = shelljs.echo(x.content).exec('dhall-to-json', { silent: true })
+      process.stdout.write = process.stderr.write = stdwriteOriginal
+      return (JSON.parse(stdout))
+    }
+  ]
+  ]
+))
 
-const load =
-  R("pipe")
-    ( getDocument
-    , parseDocument
-    )
+const load = R('pipe')(
+  getDocument,
+  parseDocument
+)
 
 module.exports = load
-//url="https://github.com/well-typed/cborg/raw/master/cborg/tests/test-vectors/deriving/a-newtype"
-//url="https://github.com/ipld/js-dag-cbor/raw/master/test/fixtures/obj-with-link.cbor"
-//url="./spec/obj-with-link.cbor"
-//load(url).then(console.log)
+// url="https://github.com/well-typed/cborg/raw/master/cborg/tests/test-vectors/deriving/a-newtype"
+// url="https://github.com/ipld/js-dag-cbor/raw/master/test/fixtures/obj-with-link.cbor"
+// url="./spec/obj-with-link.cbor"
+// load(url).then(console.log)
--- spec/test.js
+++ spec/test.js
@@ -1,36 +1,38 @@
-const load = require("../index.js")
-const t    = require("tap")
+const load = require('../index.js')
+const t = require('tap')
 
 const urls =
-  { json: "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/examples/v3.0/petstore.json"
-  , yaml: "https://raw.githubusercontent.com/gothinkster/realworld/main/api/openapi.yml"
-  , yml: "https://raw.githubusercontent.com/gothinkster/realworld/main/api/openapi.yml"
-//  , cbor: "https://github.com/ipld/js-dag-cbor/raw/master/test/fixtures/obj-with-link.cbor"
-  , dhall: "https://raw.githubusercontent.com/purescript/spago/2a70306d87ddb2a7a61cf5ac61fccd7d91ecae6c/templates/packages.dhall"
+  {
+    json: 'https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/examples/v3.0/petstore.json',
+    yaml: 'https://raw.githubusercontent.com/gothinkster/realworld/main/api/openapi.yml',
+    yml: 'https://raw.githubusercontent.com/gothinkster/realworld/main/api/openapi.yml',
+    //  , cbor: "https://github.com/ipld/js-dag-cbor/raw/master/test/fixtures/obj-with-link.cbor"
+    dhall: 'https://raw.githubusercontent.com/purescript/spago/2a70306d87ddb2a7a61cf5ac61fccd7d91ecae6c/templates/packages.dhall'
   }
 
 const filePaths =
-  { json: "./package.json"
-  , yaml: "./node_modules/tap/node_modules/shell-quote/.github/workflows/nodejs.yml"
-//  , yml: "https://raw.githubusercontent.com/gothinkster/realworld/main/api/openapi.yml"
-  , cbor: "./spec/obj-with-link.cbor"
-//  , dhall: "https://raw.githubusercontent.com/purescript/spago/2a70306d87ddb2a7a61cf5ac61fccd7d91ecae6c/templates/packages.dhall"
+  {
+    json: './package.json',
+    yaml: './node_modules/tap/node_modules/shell-quote/.github/workflows/nodejs.yml',
+    //  , yml: "https://raw.githubusercontent.com/gothinkster/realworld/main/api/openapi.yml"
+    cbor: './spec/obj-with-link.cbor'
+    //  , dhall: "https://raw.githubusercontent.com/purescript/spago/2a70306d87ddb2a7a61cf5ac61fccd7d91ecae6c/templates/packages.dhall"
   }
 
 const resultIsObject =
-  async ( x ) =>
-    { const result = await load( x.url )
-      t.equal(typeof result, "object", "Type \"." + x.type + "\" should load as object")
-    }
+  async (x) => {
+    const result = await load(x.url)
+    t.equal(typeof result, 'object', 'Type ".' + x.type + '" should load as object')
+  }
 
-t.test("Remote Tests", async t => {
-  Object.entries(urls).forEach( async x => {
-    await resultIsObject( { type: x[0], url: x[1]})
+t.test('Remote Tests', async t => {
+  Object.entries(urls).forEach(async x => {
+    await resultIsObject({ type: x[0], url: x[1] })
   })
 })
 
-t.test("Local Tests", async t => {
-  Object.entries(filePaths).forEach( async x => {
-    await resultIsObject( { type: x[0], url: x[1]})
+t.test('Local Tests', async t => {
+  Object.entries(filePaths).forEach(async x => {
+    await resultIsObject({ type: x[0], url: x[1] })
   })
 })
